464909b5aad4002b0352fe041c00599888962128
Hiten Mungara <103020212+Hitenp21@users.noreply.github.com>
2025-11-18 16:23:34 +0530

    cors add

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,14 @@ import { apiReference } from '@scalar/nestjs-api-reference';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      "https://sales-summit-io.vercel.app"
+    ],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type, Authorization",
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   // Define the Swagger options and document
