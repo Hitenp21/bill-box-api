080bac2cfcef3e5fbbf1891217341bf95b85e5b4
Hiten Mungara <103020212+Hitenp21@users.noreply.github.com>
2025-11-18 17:42:11 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,16 @@ import { apiReference } from '@scalar/nestjs-api-reference';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: ["https://bill-box-api.vercel.app"],
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-    allowedHeaders: "Content-Type, Authorization",
-    credentials: true,
-  });
+ app.enableCors({
+  origin: [
+    "https://sales-summit-io.vercel.app",
+    /\.vercel\.app$/, // optional for previews
+  ],
+  methods: ["GET","HEAD","PUT","PATCH","POST","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+});
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   // Define the Swagger options and document
