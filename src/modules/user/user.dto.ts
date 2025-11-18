577f0ebbcc23c0f4973b24843dc26961029cbf5e
Hiten Mungara <103020212+Hitenp21@users.noreply.github.com>
2025-11-18 17:19:27 +0530

    Update user.dto.ts

--- src/modules/user/user.dto.ts
+++ src/modules/user/user.dto.ts
@@ -50,4 +50,21 @@ export class CreateUserDto {
   permissions?: string[];
 }
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @ApiPropertyOptional({ example: 'john.doe@example.com', description: 'Unique email address of the user' })
+  @IsEmail()
+  @IsOptional()
+  email: string;
+
+  @ApiPropertyOptional({ example: 'John Doe', description: 'Full name of the user' })
+  @IsString()
+  @IsOptional()
+  name: string;
+
+  @ApiPropertyOptional({ example: '+919876543210', description: 'Phone number of the user' })
+  @IsOptional()
+  @IsString()
+  phoneNumber?: string;
+}
+
+
