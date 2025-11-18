da38b3da8a48ccc47ba948354a5b88358af0db3a
Hiten Mungara <103020212+Hitenp21@users.noreply.github.com>
2025-11-18 17:13:42 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      "https://sales-summit-io.vercel.app/"
-    ],
+    origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     allowedHeaders: "Content-Type, Authorization",
     credentials: true,
